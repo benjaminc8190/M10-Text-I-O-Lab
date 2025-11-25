05240d2fda54cd9cc91df1bf4ee8026a0e4e7180
benjaminc8190 <benjaminc8190@gmail.com>
2025-11-25 11:45:48 -0500

    mistake for loop

--- TextTester.java
+++ TextTester.java
@@ -8,19 +8,20 @@
 
 import java.io.*;
 
-public class TestTester{
+public class TextTester{
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException{
     int num;
     BufferedWriter output = new BufferedWriter(new FileWriter("temp.txt"));
     for (int i = 0; i < 10; i++) {
         num = (int)(Math.random() * 200) - 100;
-        output.write(Integer.toString(num));
-        output.newLine();
+        output.write(num+" ");
     }
     output.close();
 
     BufferedReader input = new BufferedReader(new FileReader("temp.txt"));
+
+    System.out.println(input.nextInt());
 
     input.close();
   }
