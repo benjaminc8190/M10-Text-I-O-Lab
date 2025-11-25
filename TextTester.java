ad127e3ad29ef803bddf986f9060f1ea0dea1199
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-11-25 11:25:29 -0500

    writing part done reading left
    Refactored the code to use BufferedWriter for writing integers to a file, and added reading functionality.

--- TextTester.java
+++ TextTester.java
@@ -5,11 +5,23 @@
 // * Author: Benjamin Chau
 // * Date: 11/25/2025
 // */
-public class TestTester{
-  PrintWriter output = new PrintWriter("temp.txt");
-  for (int i=0; i<10; i++){
-    output.writeInt((int)(Math.random()*200)-100);
-  }
 
-  output.close();
+import java.io.*;
+
+public class TestTester{
+
+  public static void main(String[] args){
+    int num;
+    BufferedWriter output = new BufferedWriter(new FileWriter("temp.txt"));
+    for (int i = 0; i < 10; i++) {
+        num = (int)(Math.random() * 200) - 100;
+        output.write(Integer.toString(num));
+        output.newLine();
+    }
+    output.close();
+
+    BufferedReader input = new BufferedReader(new FileReader("temp.txt"));
+
+    input.close();
+  }
 }
